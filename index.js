3a6baff0ae6005655c66aa40d8f0a9f9382e57a5
rbbinit <krbbinit@gmail.com>
2019-06-09 02:59:21 +0900

    #2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert('im working im js, im beautiful');
+const bb = {
+    name:"a",
+    age :33,
+    gender:"male",
+    moives : ["hoho", "aa"],
+    food : [ {name:"kimchi", fatty:false}, 
+    {name:"bur", fatty:true}]
+    }
+//object안에 array가 있네
+//rule
+// , 를 빼먹지말기
+
+console.log(bb.gender);
+bb.age= 32; //change가능
+console.log(bb);
