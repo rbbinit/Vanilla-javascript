181d367daf16fe9b6db5213f6d3e7b9a39e2ebb2
rbbinit <krbbinit@gmail.com>
2019-06-09 16:28:23 +0900

    #3

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const bb = {
-    name:"a",
-    age :33,
-    gender:"male",
-    moives : ["hoho", "aa"],
-    food : [ {name:"kimchi", fatty:false}, 
-    {name:"bur", fatty:true}]
-    }
-//object안에 array가 있네
-//rule
-// , 를 빼먹지말기
+function sayHello(potato, oi){
+    console.log("hello", potato, "i like", oi);
+}
 
-console.log(bb.gender);
-bb.age= 32; //change가능
-console.log(bb);
+sayHello("hs", 15);
+console.log("hi");
