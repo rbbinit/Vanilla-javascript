e00e724ff050919ef0b05b2711f5a97191562c5b
rbbinit <krbbinit@gmail.com>
2019-06-09 17:00:27 +0900

    #4
    function
    return

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 function sayHello(potato, oi){
-    console.log("hello", potato, "i like", oi);
+    console.log(`hello ${potato} this is good ${oi} good`);
 }
 
-sayHello("hs", 15);
-console.log("hi");
+//return으로해보자
+function ho(potato, oi)
+{
+    return `hello ${potato} this is good ${oi} good`;
+}
+
+//const hho = sayHello("ho", 23);
+const hs= ho("cheese", 30);
+console.log(ho("cheese", 30)); //hs넣어도되고
+
+const calculator = {
+    plus : function(a, b){
+    return a + b;
+    },
+    minus : function(a, b){
+        return a-b;
+    },
+    multiply: function(a, b){
+        return a*b;
+    },
+    division : function(a, b){
+        return a/b;
+    }
+}
+
+const plus = calculator.plus(5, 5);
+const minus = calculator.minus(5, 3);
+const mul = calculator.multiply(3, 3);
+const div = calculator.division(5, 2);
+console.log(plus, minus, mul, div);
