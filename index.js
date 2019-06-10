2a363f0840b35d32a850484047547e89f9ac1ffe
rbbinit <krbbinit@gmail.com>
2019-06-10 17:40:37 +0900

    #8 toggle
    엄청간편!

--- index.js
+++ index.js
@@ -3,23 +3,7 @@ const title= document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-    const currentClass= title.className;
-    const hasClass= title.classList.contains(CLICKED_CLASS);
-    if(hasClass) {
-        title.classList.remove(CLICKED_CLASS);
-    }
-    else
-    {
-        title.classList.add(CLICKED_CLASS);
-    }
-
-
-    //if(currentClass !== CLICKED_CLASS)
-    //{
-     //   title.className= CLICKED_CLASS;
-    //} else {
-     //   title.className= "";
-    //}
+ title.classList.toggle(CLICKED_CLASS);
 }
 
 
@@ -28,14 +12,3 @@ function init(){
     //mouseenter 마우스가 들어갈때마다 반응함
 }
 init();
-
-function handleOffline(){
-    console.log("lalala");
-}
-
-function handleOnline(){
-    console.log("welcome  ");
-}
-
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
