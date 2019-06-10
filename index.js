e374a67fb84ee0eab4c499e1411f0d5a20900c56
rbbinit <krbbinit@gmail.com>
2019-06-10 16:24:54 +0900

    #5
    js로 html, css조작할수있다
    
    또한 addEVentListener로
    resize, click event발생

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-function sayHello(potato, oi){
-    console.log(`hello ${potato} this is good ${oi} good`);
+const title= document.getElementById("title");
+title.innerHTML= "hi i am hs";
+title.style.color= 'red';
+//js로 html을 조작할 수 있다.
+const titles = document.querySelectorAll("#title");
+
+function handleResize(event){
+    console.log(event)
 }
 
-//return으로해보자
-function ho(potato, oi)
-{
-    return `hello ${potato} this is good ${oi} good`;
+window.addEventListener("resize", handleResize);
+//call function
+console.log(title);
+
+function handleClick(){
+    title.style.color= "blue";
 }
-
-//const hho = sayHello("ho", 23);
-const hs= ho("cheese", 30);
-console.log(ho("cheese", 30)); //hs넣어도되고
-
-const calculator = {
-    plus : function(a, b){
-    return a + b;
-    },
-    minus : function(a, b){
-        return a-b;
-    },
-    multiply: function(a, b){
-        return a*b;
-    },
-    division : function(a, b){
-        return a/b;
-    }
-}
-
-const plus = calculator.plus(5, 5);
-const minus = calculator.minus(5, 3);
-const mul = calculator.multiply(3, 3);
-const div = calculator.division(5, 2);
-console.log(plus, minus, mul, div);
+title.addEventListener("click", handleClick);
